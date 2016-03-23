3f651b1d9411b22643ef4e9f8cdbb78da5d43a2f
Nasz <nasihere@gmail.com>
2016-03-23 11:34:28 -0700

    shared

--- angularApp2/angular2-seed/app/author.service.ts
+++
@@ -1,6 +0,0 @@
-
-export class AuthorService {
-    getAuthors() : Array<string> {
-        return ['Author1','Author2','Author3'];
-    }
-}
--- angularApp2/angular2-seed/app/course.service.ts
+++
@@ -1,6 +0,0 @@
-
-export class CourseService {
-    getCourses() : Array<string> {
-        return ['Course1','Course2','Course3'];
-    }
-}
